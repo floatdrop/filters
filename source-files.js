14f267194b88277b15732afe5de270bf69c0c99c
Travis CI User <travis@example.org>
2019-12-11 14:08:32 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["filters"] = {"name":"","dirs":[{"name":"failable","dirs":[{"name":"ops","files":["and.rs","bool.rs","map.rs","mod.rs","not.rs","or.rs","xor.rs"]}],"files":["filter.rs","mod.rs"]},{"name":"ops","files":["and.rs","bool.rs","failable.rs","map.rs","mod.rs","not.rs","or.rs","xor.rs"]}],"files":["filter.rs","impl_traits.rs","iter.rs","lib.rs"]};
+createSourceSidebar();
